876aa38f56fa96635faa69df1d6d8ebee1900391
engelsun <artyom.romenskiy@gmail.com>
2017-05-04 15:43:57 +0300

    task 3110ch5

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/Command.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/Command.java
@@ -1,8 +1,5 @@
 package com.javarush.task.task31.task3110.command;
 
-/**
- * Created by engelsun on 5/4/2017.
- */
 public interface Command {
     void execute() throws Exception;
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task31.task3110.command;
+
+/**
+ * Created by engelsun on 5/4/2017.
+ */
+public abstract class ZipCommand implements Command {
+}
