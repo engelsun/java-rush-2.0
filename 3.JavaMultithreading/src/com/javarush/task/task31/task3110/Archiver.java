d3eeb1fc2a468474d1f8c01201dd06bc46011c91
engelsun <artyom.romenskiy@gmail.com>
2017-05-04 11:49:26 +0300

    task 3110ch1

---
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/Archiver.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task31.task3110;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+
+/**
+ * Created by engelsun on 5/4/2017.
+ */
+public class Archiver {
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Enter an absolute path to archive");
+        String pathToZipFile = reader.readLine();
+        ZipFileManager zipFileManager = new ZipFileManager(Paths.get(pathToZipFile));
+
+        System.out.println("Enter an absolute path to the file which you wish to archive");
+        String pathToFile = reader.readLine();
+        zipFileManager.createZip(Paths.get(pathToFile));
+
+    }
+}
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,7 +1,18 @@
 package com.javarush.task.task31.task3110;
 
+import java.nio.file.Path;
+
 /**
  * Created by engelsun on 5/4/2017.
  */
 public class ZipFileManager {
+    private Path zipFile;
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+
+    public void createZip(Path source) throws Exception {
+
+    }
 }
