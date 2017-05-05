6e4273264cfae6abf599b33747394c1957eb2821
engelsun <artyom.romenskiy@gmail.com>
2017-05-05 13:02:31 +0300

    task 3110ch10

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/FileManager.java
@@ -7,9 +7,6 @@ import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by engelsun on 5/4/2017.
- */
 public class FileManager {
     private Path rootPath;
     private List<Path> fileList;
@@ -25,15 +22,19 @@ public class FileManager {
     }
 
     private void collectFileList(Path path) throws IOException {
+        // Добавляем только файлы
         if (Files.isRegularFile(path)) {
-            fileList.add(rootPath.relativize(path));
+            Path relativePath = rootPath.relativize(path);
+            fileList.add(relativePath);
         }
 
+        // Добавляем содержимое директории
         if (Files.isDirectory(path)) {
+            // Рекурсивно проходимся по всему содержмому директории
+            // Чтобы не писать код по вызову close для DirectoryStream, обернем вызов newDirectoryStream в try-with-resources
             try (DirectoryStream<Path> directoryStream = Files.newDirectoryStream(path)) {
-                for (Path filePath :
-                        directoryStream) {
-                    collectFileList(filePath);
+                for (Path file : directoryStream) {
+                    collectFileList(file);
                 }
             }
         }
--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/ZipFileManager.java
@@ -1,8 +1,13 @@
 package com.javarush.task.task31.task3110;
 
+import com.javarush.task.task31.task3110.exception.PathIsNotFoundException;
+
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
@@ -17,19 +22,44 @@ public class ZipFileManager {
     }
 
     public void createZip(Path source) throws Exception {
+        Path zipDirectory = zipFile.getParent();
+        if (Files.notExists(zipDirectory)) {
+            Files.createDirectory(zipDirectory);
+        }
+
         try (ZipOutputStream zipOutputStream = new ZipOutputStream(Files.newOutputStream(zipFile))) {
-            String fileName = source.getFileName().toString();
-            ZipEntry zipEntry = new ZipEntry(fileName);
+            if (Files.isRegularFile(source)) {
+                addNewZipEntry(zipOutputStream, source.getParent(), source.getFileName());
+            } else if (Files.isDirectory(source)) {
+                FileManager fileManager = new FileManager(source);
+                List<Path> fileNames = fileManager.getFileList();
+
+                for (Path fileName : fileNames) {
+                    addNewZipEntry(zipOutputStream, source, fileName);
+                }
+            } else {
+                throw new PathIsNotFoundException();
+            }
+        }
+    }
+
+    private void addNewZipEntry(ZipOutputStream zipOutputStream, Path filePath, Path fileName) throws Exception {
+        try (InputStream inputStream = Files.newInputStream(filePath.resolve(fileName))) {
+            ZipEntry zipEntry = new ZipEntry(fileName.toString());
             zipOutputStream.putNextEntry(zipEntry);
 
-            try (InputStream inputStream = Files.newInputStream(source)) {
-                int length;
-                byte[] buffer = new byte[8 * 1024];
-                while ((length = inputStream.read(buffer)) > 0) {
-                    zipOutputStream.write(buffer, 0, length);
-                }
-            }
+            copyData(inputStream, zipOutputStream);
+
             zipOutputStream.closeEntry();
+        }
+
+    }
+
+    private void copyData(InputStream in, OutputStream out) throws IOException {
+        int length;
+        byte[] buffer = new byte[8 * 1024];
+        while ((length = in.read(buffer)) > 0) {
+            out.write(buffer, 0, length);
         }
     }
 }
