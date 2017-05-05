eff40e8fcc69e9dbe6e17ddb83addb1428f885b7
engelsun <artyom.romenskiy@gmail.com>
2017-05-05 18:38:13 +0300

    task 3110ch11

--- 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task31/task3110/command/ZipCommand.java
@@ -1,7 +1,18 @@
 package com.javarush.task.task31.task3110.command;
 
+import com.javarush.task.task31.task3110.ConsoleHelper;
+import com.javarush.task.task31.task3110.ZipFileManager;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Created by engelsun on 5/4/2017.
  */
 public abstract class ZipCommand implements Command {
+    public ZipFileManager getZipFileManager() throws Exception {
+        ConsoleHelper.writeMessage("Введите полный путь файла архива");
+        Path zipPath = Paths.get(ConsoleHelper.readString());
+        return new ZipFileManager(zipPath);
+    }
 }
