38f995349ae38de62c17f1a5567268e6607e16fe
engelsun <artyom.romenskiy@gmail.com>
2017-05-16 16:32:07 +0300

    task 2213ch1

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Figure.java
@@ -3,8 +3,5 @@ package com.javarush.task.task22.task2213;
 /**
  * Created by engelsun on 5/16/2017.
  */
-public class Tetris {
-    public static void main(String[] args) {
-
-    }
+public class Figure {
 }
