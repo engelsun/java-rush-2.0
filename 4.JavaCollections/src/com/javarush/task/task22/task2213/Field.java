186b562ab23e44c8711f306bbc694977d00de9fc
engelsun <artyom.romenskiy@gmail.com>
2017-05-16 19:54:10 +0300

    task 2213ch6

--- 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/Field.java
@@ -4,4 +4,25 @@ package com.javarush.task.task22.task2213;
  * Created by engelsun on 5/16/2017.
  */
 public class Field {
+    private int width;
+    private int height;
+    private int[][] matrix;
+
+    public Field(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix = new int[height][width];
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int[][] getMatrix() {
+        return matrix;
+    }
 }
