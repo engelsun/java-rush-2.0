ad053cf4335cec1553e0a47650a4adccf55b5b0f
engelsun <artyom.romenskiy@gmail.com>
2017-05-16 20:18:19 +0300

    task 2213ch11

---
+++ 4.JavaCollections/src/com/javarush/task/task22/task2213/FigureFactory.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task22.task2213;
+
+/**
+ * Created by engelsun on 5/16/2017.
+ */
+public class FigureFactory {
+    public static Figure createRandomFigure(int x, int y) {
+        return null;
+    }
+}
