df0b8ed0150843c9921a9eb11c7f8d0a77b4fe20
engelsun <artyom.romenskiy@gmail.com>
2017-05-20 15:56:42 +0300

    task 2515ch1

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -3,8 +3,5 @@ package com.javarush.task.task25.task2515;
 /**
  * Created by engelsun on 5/20/2017.
  */
-public class Space {
-    public static void main(String[] args) {
-
-    }
+public class SpaceShip {
 }
