7aabac92aa34ebd5b2be5260d939f17a550f4bdd
engelsun <artyom.romenskiy@gmail.com>
2017-05-20 16:06:51 +0300

    task 2515ch2

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Canvas.java
@@ -3,5 +3,5 @@ package com.javarush.task.task25.task2515;
 /**
  * Created by engelsun on 5/20/2017.
  */
-public class SpaceShip extends BaseObject {
+public class Canvas {
 }
--- 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/Ufo.java
@@ -3,5 +3,5 @@ package com.javarush.task.task25.task2515;
 /**
  * Created by engelsun on 5/20/2017.
  */
-public class Ufo {
+public class Ufo extends BaseObject {
 }
