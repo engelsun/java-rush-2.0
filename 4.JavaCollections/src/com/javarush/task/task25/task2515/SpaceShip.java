560d5cf1e8a4d1c2976c5deb151c2df7d5ba206b
engelsun <artyom.romenskiy@gmail.com>
2017-05-21 15:24:06 +0300

    task 2515ch13

--- 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
+++ 4.JavaCollections/src/com/javarush/task/task25/task2515/SpaceShip.java
@@ -17,4 +17,22 @@ public class SpaceShip extends BaseObject {
     public void moveRight() {
         dx = 1;
     }
+
+    @Override
+    public void move() {
+        x += dx;
+        checkBorders(0, Space.game.getWidth(), 0, Space.game.getHeight());
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    public void fire() {
+        Rocket rocket1 = new Rocket(x - 2, y);
+        Rocket rocket2 = new Rocket(x + 2, y);
+        Space.game.getRockets().add(rocket1);
+        Space.game.getRockets().add(rocket2);
+    }
 }
