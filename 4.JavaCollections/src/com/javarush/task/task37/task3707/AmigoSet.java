8d82025e7da75d91d1904d8b615ebbe599abc6a5
engelsun <artyom.romenskiy@gmail.com>
2017-05-25 12:14:48 +0300

    task 3707ch1

--- 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
+++ 4.JavaCollections/src/com/javarush/task/task37/task3707/AmigoSet.java
@@ -1,7 +1,46 @@
 package com.javarush.task.task37.task3707;
 
+import java.io.Serializable;
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+
 /**
  * Created by engelsun on 5/25/2017.
  */
-public class AmigoSet {
+public class AmigoSet<E> extends AbstractSet<E> implements Set<E>, Serializable, Cloneable{
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return super.contains(o);
+    }
+
+    @Override
+    public boolean add(E e) {
+        return super.add(e);
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return super.remove(o);
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
