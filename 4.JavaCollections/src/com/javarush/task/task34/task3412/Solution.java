24437d9fc1344e72ea6b08c8f2b128c194155831
engelsun <artyom.romenskiy@gmail.com>
2017-05-25 11:51:30 +0300

    task 3412

---
+++ 4.JavaCollections/src/com/javarush/task/task34/task3412/Solution.java
@@ -0,0 +1,78 @@
+package com.javarush.task.task34.task3412;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Date;
+
+/* 
+Добавление логирования в класс
+*/
+
+public class Solution {
+    private static final Logger logger = LoggerFactory.getLogger(Solution.class);
+
+    private int value1;
+    private String value2;
+    private Date value3;
+
+    public Solution(int value1, String value2, Date value3) {
+        logger.debug("init constructor > param > 1 " + this.value1 + " 2 " + this.value2 + " 3 " + this.value2);
+        this.value1 = value1;
+        this.value2 = value2;
+        this.value3 = value3;
+    }
+
+    public static void main(String[] args) {
+    }
+
+    public void calculateAndSetValue3(long value) {
+        logger.trace("starts calculateAndSetValue3");
+        value -= 133;
+        if (value > Integer.MAX_VALUE) {
+            value1 = (int) (value / Integer.MAX_VALUE);
+            logger.debug("set value1:" + value1);
+        } else {
+            value1 = (int) value;
+            logger.debug("set value1:" + value1);
+        }
+    }
+
+    public void printString() {
+        logger.trace("starts printingString");
+        if (value2 != null) {
+            System.out.println(value2.length());
+        }
+    }
+
+    public void printDateAsLong() {
+        logger.trace("starts printDateAsLong");
+        if (value3 != null) {
+            System.out.println(value3.getTime());
+        }
+    }
+
+    public void divide(int number1, int number2) {
+        logger.trace("starts divide");
+        try {
+            System.out.println(number1 / number2);
+        } catch (ArithmeticException e) {
+            logger.error("exception: " + e);
+        }
+    }
+
+    public void setValue1(int value1) {
+        logger.debug("set value1:" + value1);
+        this.value1 = value1;
+    }
+
+    public void setValue2(String value2) {
+        logger.debug("set value2:" + value2);
+        this.value2 = value2;
+    }
+
+    public void setValue3(Date value3) {
+        logger.debug("set value3:" + value3);
+        this.value3 = value3;
+    }
+}
