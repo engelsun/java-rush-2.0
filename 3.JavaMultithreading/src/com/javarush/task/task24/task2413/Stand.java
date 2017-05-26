94eaba179a61b572cb3f62c63fa0c28f50ce4f79
engelsun <artyom.romenskiy@gmail.com>
2017-05-26 17:26:40 +0300

    task 2413ch16

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Stand.java
@@ -4,8 +4,21 @@ package com.javarush.task.task24.task2413;
  * Created by engelsun on 5/25/2017.
  */
 public class Stand extends BaseObject {
-    public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed;
+    private double direction;
+
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public double getDirection() {
+        return direction;
     }
 
     @Override
