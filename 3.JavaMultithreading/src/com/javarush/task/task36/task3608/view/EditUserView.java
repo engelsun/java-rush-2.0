5f27f3c5d28e8ef228655e42e7e05b9ac9ebcd09
engelsun <artyom.romenskiy@gmail.com>
2017-05-26 23:44:07 +0300

    task 3608ch6

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/view/EditUserView.java
@@ -7,7 +7,7 @@ import com.javarush.task.task36.task3608.model.ModelData;
 /**
  * Created by engelsun on 5/26/2017.
  */
-public class UsersView implements View {
+public class EditUserView implements View {
     private Controller controller;
 
     public void setController(Controller controller) {
@@ -16,20 +16,9 @@ public class UsersView implements View {
 
     @Override
     public void refresh(ModelData modelData) {
-        if (modelData.isDisplayDeletedUserList()) System.out.println("All deleted users:");
-        else System.out.println("All users:");
-        for (User user :
-                modelData.getUsers()) {
-            System.out.println("\t" + user);
-        }
+        System.out.println("User to be edited:");
+        User user = modelData.getActiveUser();
+        System.out.println("\t" + user);
         System.out.println("===================================================");
-    }
-
-    public void fireEventShowAllUsers() {
-        controller.onShowAllUsers();
-    }
-
-    public void fireEventShowDeletedUsers() {
-        controller.onShowAllDeletedUsers();
     }
 }
