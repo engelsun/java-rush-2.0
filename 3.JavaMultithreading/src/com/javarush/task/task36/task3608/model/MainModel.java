9d317b3443f2d28b88d449781b448ffdab81e1ea
engelsun <artyom.romenskiy@gmail.com>
2017-05-26 22:43:26 +0300

    task 3608ch4

---
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/model/MainModel.java
@@ -0,0 +1,26 @@
+package com.javarush.task.task36.task3608.model;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.model.service.UserService;
+import com.javarush.task.task36.task3608.model.service.UserServiceImpl;
+
+import java.util.List;
+
+/**
+ * Created by engelsun on 5/26/2017.
+ */
+public class MainModel implements Model {
+    private ModelData modelData = new ModelData();
+    private UserService userService = new UserServiceImpl();
+
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+
+    @Override
+    public void loadUsers() {
+        List<User> users = userService.getUsersBetweenLevels(1, 100);
+        modelData.setUsers(users);
+    }
+}
