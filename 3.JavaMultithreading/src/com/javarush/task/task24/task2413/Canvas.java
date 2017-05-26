5f5e4d8ac8a1fd1902b19a811bc5cba78febb767
engelsun <artyom.romenskiy@gmail.com>
2017-05-26 14:38:46 +0300

    task 2413ch10

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Canvas.java
@@ -37,4 +37,23 @@ public class Canvas {
     public void setMatrix(char[][] matrix) {
         this.matrix = matrix;
     }
+
+    public void setPoint(double x, double y, char c) {
+        int roundX = (int) Math.round(x);
+        int roundY = (int) Math.round(y);
+
+        if (roundX > 0 && roundX < matrix[0].length && roundY > 0 && roundY < matrix.length) {
+            matrix[roundY][roundX] = c;
+        }
+    }
+
+    public void drawMatrix(double x, double y, int[][] matrix, char c) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] != 0) {
+                    setPoint(x + j, y + i, c);
+                }
+            }
+        }
+    }
 }
