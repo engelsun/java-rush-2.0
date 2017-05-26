80b6aa02114ecf1bca1f4a4838c296ebe77ac579
engelsun <artyom.romenskiy@gmail.com>
2017-05-26 13:59:48 +0300

    task 2413ch7

--- 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
+++ 3.JavaMultithreading/src/com/javarush/task/task24/task2413/Ball.java
@@ -4,4 +4,7 @@ package com.javarush.task.task24.task2413;
  * Created by engelsun on 5/25/2017.
  */
 public class Ball extends BaseObject {
+    public Ball(double x, double y, double radius) {
+        super(x, y, radius);
+    }
 }
