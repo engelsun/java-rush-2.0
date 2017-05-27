87ea4e1f8f05e544ee735b1a5d4f9c56b8ad43df
engelsun <artyom.romenskiy@gmail.com>
2017-05-27 16:05:45 +0300

    task 2909ch1

--- 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
+++ 3.JavaMultithreading/src/com/javarush/task/task29/task2909/human/Human.java
@@ -1,11 +1,16 @@
 package com.javarush.task.task29.task2909.human;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Human {
     public static int nextId = 0;
     private int id;
     protected int age;
     protected String name;
     protected int course;
+    private List<Human> children = new ArrayList<>();
 
     protected int[] size;
 
@@ -69,5 +74,17 @@ public class Human {
 
     public void printSize() {
         System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+    }
+
+    public List<Human> getChildren() {
+        return Collections.unmodifiableList(children);
+    }
+
+    public void addChild(Human human) {
+        children.add(human);
+    }
+
+    public void removeChild(Human human) {
+        children.remove(human);
     }
 }
