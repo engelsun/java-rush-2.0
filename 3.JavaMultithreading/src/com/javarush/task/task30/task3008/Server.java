2147304bec3397cd27f81a9bf831e8ac8eccff5d
engelsun <artyom.romenskiy@gmail.com>
2017-05-29 21:21:21 +0300

    task 3008ch6

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,9 +1,32 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Created by engelsun on 5/29/2017.
  */
 public class Server {
-    public static void main(String[] args) {
+    private static class Handler extends Thread {
+        private Socket socket;
+
+        public Handler(Socket socket) {
+            this.socket = socket;
+        }
+
+    }
+    public static void main(String[] args) throws IOException {
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket serverSocket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен");
+            while (true) {
+                    Socket socket = serverSocket.accept();
+                    Handler handler = new Handler(socket);
+                    handler.start();
+            }
+        } catch (Exception e) {
+            ConsoleHelper.writeMessage(e.getMessage());
+        }
     }
 }
