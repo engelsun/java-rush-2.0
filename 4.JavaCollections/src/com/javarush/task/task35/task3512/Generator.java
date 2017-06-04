ad672b95b19227e082b687a85dea0f70bd8f631c
engelsun <artyom.romenskiy@gmail.com>
2017-06-04 19:56:52 +0300

    task 3512

---
+++ 4.JavaCollections/src/com/javarush/task/task35/task3512/Generator.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task35.task3512;
+
+public class Generator<T> {
+    private Class<T> t;
+
+    public Generator(Class<T> t) {
+        this.t = t;
+    }
+
+    T newInstance() throws IllegalAccessException, InstantiationException {
+        return t.newInstance();
+    }
+}
