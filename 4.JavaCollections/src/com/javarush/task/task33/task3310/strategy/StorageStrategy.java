1d6325a191c5a7aae5d4cc960218ad353803ab11
engelsun <artyom.romenskiy@gmail.com>
2017-06-11 20:45:59 +0300

    task 3310ch2

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/strategy/StorageStrategy.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task33.task3310.strategy;
+
+/**
+ * Created by engelsun on 6/11/2017.
+ */
+public interface StorageStrategy {
+    boolean containsKey(Long key);
+
+    boolean containsValue(String value);
+
+    void put(Long id, String value);
+
+    Long getKey(String value);
+
+    String getValue(Long key);
+}
