84b77fc0526d7726b689da4642cc8eafbbfa92c6
engelsun <artyom.romenskiy@gmail.com>
2017-06-11 20:40:24 +0300

    task 3310ch1

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -4,4 +4,11 @@ package com.javarush.task.task33.task3310;
  * Created by engelsun on 6/11/2017.
  */
 public class Shortener {
+    public Long getId(String string) {
+        return 0L;
+    }
+
+    public String getString(Long id) {
+        return null;
+    }
 }
---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Solution.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by engelsun on 6/11/2017.
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
