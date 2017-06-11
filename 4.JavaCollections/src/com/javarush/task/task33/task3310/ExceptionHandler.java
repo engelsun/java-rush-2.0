7e086061644a6fa4cb6732c44ad559bfef143409
engelsun <artyom.romenskiy@gmail.com>
2017-06-11 21:19:14 +0300

    task 3310ch4

---
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/ExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task33.task3310;
+
+/**
+ * Created by engelsun on 6/11/2017.
+ */
+public class ExceptionHandler {
+    public static void log(Exception e) {
+        Helper.printMessage(e.getMessage());
+    }
+}
