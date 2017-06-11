0d52a4cceae35990945d1c6a125288f1aeea5aca
engelsun <artyom.romenskiy@gmail.com>
2017-06-11 21:04:51 +0300

    task 3310ch3

--- 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
+++ 4.JavaCollections/src/com/javarush/task/task33/task3310/Shortener.java
@@ -1,14 +1,28 @@
 package com.javarush.task.task33.task3310;
 
+import com.javarush.task.task33.task3310.strategy.StorageStrategy;
+
 /**
  * Created by engelsun on 6/11/2017.
  */
 public class Shortener {
-    public Long getId(String string) {
-        return 0L;
+    private Long lastId = 0L;
+    private StorageStrategy storageStrategy;
+
+    public Shortener(StorageStrategy storageStrategy) {
+        this.storageStrategy = storageStrategy;
     }
 
-    public String getString(Long id) {
-        return null;
+    public synchronized Long getId(String string) {
+        if (storageStrategy.containsValue(string)) return storageStrategy.getKey(string);
+        else {
+            lastId++;
+            storageStrategy.put(lastId, string);
+            return lastId;
+        }
+    }
+
+    public synchronized String getString(Long id) {
+        return storageStrategy.getValue(id);
     }
 }
