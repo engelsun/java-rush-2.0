5962a27b9250efd1ee6ac8ab89ace1f7f73db965
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 01:15:17 +0300

    task 2810ch8

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -3,6 +3,8 @@ package com.javarush.task.task28.task2810.model;
 import com.javarush.task.task28.task2810.vo.Vacancy;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -13,21 +15,47 @@ import java.util.List;
  */
 public class HHStrategy implements Strategy {
     private static final String URL_FORMAT = "http://hh.ua/search/vacancy?text=java+%s&page=%d";
+    //private static final String URL_FORMAT = "http://javarush.ru/testdata/big28data.html";
     String userAgent = "Mozilla/5.0";
     String referrer = "http://google.ru";
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
         List<Vacancy> list = new ArrayList<>();
-        int page = 1;
+        int page = 0;
         try {
-            Document document = Jsoup.connect(URL_FORMAT)
-                    .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
-                    .referrer("http://www.google.com")
-                    .get();
+            while (true) {
+                Document document = getDocument(searchString, page);
+                if (document == null)
+                    break;
+                page++;
+                Elements elements = document.body().getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy");
+                if (!elements.isEmpty()) {
+                    for (Element element : elements) {
+                        Vacancy vacancy = new Vacancy();
+                        vacancy.setSiteName("http://hh.ua");
+                        vacancy.setUrl(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").attr("href"));
+                        vacancy.setCity(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-address").text());
+                        vacancy.setCompanyName(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-employer").text());
+                        vacancy.setTitle(element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-title").text());
+                        String salary = element.getElementsByAttributeValue("data-qa", "vacancy-serp__vacancy-compensation").text();
+                        vacancy.setSalary(salary != null ? salary : "");
+                        list.add(vacancy);
+                    }
+                } else break;
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
         return list;
+    }
+
+    protected Document getDocument(String searchString, int page) throws IOException {
+        String url = String.format(URL_FORMAT, searchString, page);
+        return Jsoup.connect(url)
+                .userAgent("Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36")
+                .timeout(5000)
+                .referrer("http://google.ru")
+                .get();
     }
 }
