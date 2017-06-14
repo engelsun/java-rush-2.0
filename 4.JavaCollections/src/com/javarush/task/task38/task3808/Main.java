c2c48d0e2ff453807f2774b9a1519138e9531ce7
engelsun <artyom.romenskiy@gmail.com>
2017-06-14 23:18:36 +0300

    task 3808

---
+++ 4.JavaCollections/src/com/javarush/task/task38/task3808/Main.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task38.task3808;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * Created by engelsun on 6/14/2017.
+ */
+@Target(ElementType.METHOD)
+public @interface Main {
+}
