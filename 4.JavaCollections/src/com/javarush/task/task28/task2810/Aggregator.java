07874d208af10c606a5619c9ae30e44f370d1a1e
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 00:06:14 +0300

    task 2810

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -0,0 +1,10 @@
+package com.javarush.task.task28.task2810;
+
+/**
+ * Created by engelsun on 6/15/2017.
+ */
+public class Aggregator {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task28.task2810.model;
+
+/**
+ * Created by engelsun on 6/15/2017.
+ */
+public class Provider {
+
+}
