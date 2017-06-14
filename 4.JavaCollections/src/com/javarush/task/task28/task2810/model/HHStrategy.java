e774135b36276a1a00d8f844edebcc6e5259e6ed
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 00:13:16 +0300

    task 2810ch3

---
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/HHStrategy.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task28.task2810.model;
+
+/**
+ * Created by engelsun on 6/15/2017.
+ */
+public class HHStrategy implements Strategy {
+}
--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
@@ -1,5 +1,9 @@
 package com.javarush.task.task28.task2810.model;
 
+import com.javarush.task.task28.task2810.vo.Vacancy;
+
+import java.util.List;
+
 /**
  * Created by engelsun on 6/15/2017.
  */
@@ -13,4 +17,6 @@ public class Provider {
     public void setStrategy(Strategy strategy) {
         this.strategy = strategy;
     }
+
+    public List<Vacancy> getJavaVacancies(String searchString) {return null;}
 }
