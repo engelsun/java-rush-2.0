6420ffe2aea3005d95f468a939bdeddb6f91c372
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 00:07:51 +0300

    task 2810ch1

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/model/Provider.java
@@ -4,5 +4,13 @@ package com.javarush.task.task28.task2810.model;
  * Created by engelsun on 6/15/2017.
  */
 public class Provider {
+    Strategy strategy;
 
+    public Provider(Strategy strategy) {
+        this.strategy = strategy;
+    }
+
+    public void setStrategy(Strategy strategy) {
+        this.strategy = strategy;
+    }
 }
