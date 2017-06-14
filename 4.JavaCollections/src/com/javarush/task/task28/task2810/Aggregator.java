988f5b52a8256c6e9125adbf86d920d85469ca9c
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 01:33:51 +0300

    task 2810ch12

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Aggregator.java
@@ -1,12 +1,20 @@
 package com.javarush.task.task28.task2810;
 
 import com.javarush.task.task28.task2810.model.HHStrategy;
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
+import com.javarush.task.task28.task2810.view.HtmlView;
 
 /**
  * Created by engelsun on 6/15/2017.
  */
 public class Aggregator {
     public static void main(String[] args) {
+        HtmlView view = new HtmlView();
+        Provider provider = new Provider(new HHStrategy());
+        Model model = new Model(view, provider);
+        Controller controller = new Controller(model);
+        view.setController(controller);
+        view.userCitySelectEmulationMethod ();
     }
 }
