c6a793b09de104c364a94d8886d7ffe536b6d61e
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 01:30:07 +0300

    task 2810ch11

--- 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
+++ 4.JavaCollections/src/com/javarush/task/task28/task2810/Controller.java
@@ -1,39 +1,23 @@
 package com.javarush.task.task28.task2810;
 
+import com.javarush.task.task28.task2810.model.Model;
 import com.javarush.task.task28.task2810.model.Provider;
-import com.javarush.task.task28.task2810.vo.Vacancy;
 
 import java.util.Arrays;
-import java.util.List;
 
 /**
  * Created by engelsun on 6/15/2017.
  */
 public class Controller {
-    private Provider[] providers;
+    private Model model;
 
-    public Controller(Provider... providers) {
-        if (providers == null || providers.length == 0)
+    public Controller(Model model) {
+        if (model == null)
             throw new IllegalArgumentException();
-        this.providers = providers;
+        this.model = model;
     }
 
-    @Override
-    public String toString() {
-        return "Controller{" +
-                "providers=" + Arrays.toString(providers) +
-                '}';
-    }
-
-    public void scan() {
-        int total = 0;
-        if (providers == null)
-            System.out.println("Hello");
-
-        for (Provider provider : providers) {
-            List<Vacancy> vacancy = provider.getJavaVacancies("java");
-            total += vacancy.size();
-        }
-        System.out.println(total);
+    public void onCitySelect(String cityName) {
+        model.selectCity(cityName);
     }
 }
