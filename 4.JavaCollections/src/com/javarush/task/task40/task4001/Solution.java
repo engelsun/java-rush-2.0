f45a4cd6d9db195c8ebc5616216e2ef237da5d90
engelsun <artyom.romenskiy@gmail.com>
2017-06-15 13:59:01 +0300

    task 4001

---
+++ 4.JavaCollections/src/com/javarush/task/task40/task4001/Solution.java
@@ -0,0 +1,45 @@
+package com.javarush.task.task40.task4001;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/* 
+POST, а не GET
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.sendPost(new URL("https://requestb.in/152p8g41"), "name=zapp&mood=good&locale=&id=777");
+    }
+
+    public void sendPost(URL url, String urlParameters) throws Exception {
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("User-Agent", "Chrome");
+
+        connection.setDoOutput(true);
+        try (OutputStream out = connection.getOutputStream()) {
+            out.write(urlParameters.getBytes());
+        }
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("Response Code: " + responseCode);
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String responseLine;
+        StringBuilder response = new StringBuilder();
+
+        while ((responseLine = bufferedReader.readLine()) != null) {
+            response.append(responseLine);
+        }
+        bufferedReader.close();
+
+        System.out.println("Response: " + response.toString());
+    }
+}
